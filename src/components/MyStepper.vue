8bd265009b7c982367a665ebb82908d6d1604cb7
NAO5802 <i100185b@gmail.com>
2023-10-19 09:00:52 +0900

    add component test

---
+++ src/components/MyStepper.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {increment} from "@/utils/helper";
+
+export default defineComponent({
+  name: "MyStepper",
+  methods: {increment},
+  props: {
+    max: {
+      type: Number,
+      default: 5
+    },
+    current: {
+      type: Number,
+      default: 0
+    }
+  }
+})
+</script>
+
+<template>
+  <div>
+    <h1 data-testid="stepper-value">Stepper: {{ current }}</h1>
+    <button data-testid="increment" @click="increment(current, max)" type="button"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/__tests__/MyStepper.spec.ts
@@ -0,0 +1,21 @@
+import {describe, test ,expect} from "vitest";
+import {mount} from "@vue/test-utils";
+import MyStepper from "@/components/MyStepper.vue";
+
+const wrapper = mount(MyStepper, {
+  props:{
+    max: 1
+  }
+})
+describe('MyStepper', () => {
+  test('初期値が0であること',()=>{
+    expect(wrapper.find('[data-testid=stepper-value]').text()).toContain('0')
+  })
+
+  test('ボタンをクリックするとカウントが1になること',async ()=>{
+    // TODO
+    // await wrapper.find('[data-testid=increment]').trigger('click')
+    //
+    // expect(wrapper.find('[data-testid=stepper-value]').text()).toContain('1')
+  })
+})
